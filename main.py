894941ccf79e8ea07c16be5f1ff9744b715776dd
Fassinou Bill <billfass2010@gmail.com>
2023-09-19 20:42:41 +0100

    ajout du fichier main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# Bile Fassinou, Master 2 DIT promotion avril 23
+from structure import CreateStructure
+from mygit import Gitinitiate
+
+if __name__ == '__main__':
+
+    project_root = '/home/urban/Documents/DITMaster2/DEVOIR/BRANCHES/main' 
+
+    folder = CreateStructure.Structure(project_root)
+
+    folders.method_1()
+
+    strucutres = ['main']
+
+    myDict = {
+        'main': project_root,
+    }
+
+    for repository, url in myDict.items():
+        repo_path = url
+        repo_url = 'https://github.com/billfass/devoir_versioning.git'
+        branch_name = repository
+        commit_message = f'commit pour la première branche dénommée {branch_name}'
+        username = 'billfass'
+        token = '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'
+
+        myGit = Gitinitiate.Gitinit(repo_path, repo_url, commit_message, branch_name, username, token)
+        myGit.compute()
+    
+
+    
