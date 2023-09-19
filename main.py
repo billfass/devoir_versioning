3aa4b4fd6248c8fb90a76592c5fc7fe8c037f328
billfass <40564486+billfass@users.noreply.github.com>
2023-09-19 21:24:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 # Bile Fassinou, Master 2 DIT promotion avril 23
 from structure import CreateStructure
-from mygit import Gitinitiate
 
 if __name__ == '__main__':
 
-    project_root = '/home/urban/Documents/DITMaster2/DEVOIR/BRANCHES/main' 
+    project_root = '/home/urban/Documents/DITMaster2/DEVOIR/BRANCHES/development' 
 
     folder = CreateStructure.Structure(project_root)
 
-    folders.method_1()
-
-    strucutres = ['main']
-
-    myDict = {
-        'main': project_root,
-    }
-
-    for repository, url in myDict.items():
-        repo_path = url
-        repo_url = 'https://github.com/billfass/devoir_versioning.git'
-        branch_name = repository
-        commit_message = f'commit pour la première branche dénommée {branch_name}'
-        username = 'billfass'
-        token = 'votre_token'
-
-        myGit = Gitinitiate.Gitinit(repo_path, repo_url, commit_message, branch_name, username, token)
-        myGit.compute()
-    
+    folders.method_2()
 
     
